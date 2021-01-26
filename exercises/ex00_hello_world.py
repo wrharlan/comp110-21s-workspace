7071d4137d6008f439d5b5bc916e4ad09f486348
Robert Harlan <wrharlan@email.unc.edu>
2021-01-25 20:19:44 -0500

    Finished exercise 00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,4 @@
+"""My first program for comp110"""
+__author__ = "730238353"
+
+print("Hello world.")
---
+++ lessons/00_stored_program.py
@@ -0,0 +1,5 @@
+""" An example, simple program. """
+quiz: int = 100
+print(quiz)
+print("Hello World")
+print("Goodbye.")
